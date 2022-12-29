80c5bf39a645104012fac23a448c4c09753dde93
AnamShabnam <120900876+AnamShabnam@users.noreply.github.com>
2022-12-29 17:25:28 +0500

    Create Traffic signal.py

---
+++ Traffic signal.py
@@ -0,0 +1,9 @@
+light = input("Enter your Traffic signal ? ")
+if light == "green":
+    print("Car is allow to go!")
+elif light == "yellow":
+    print("Car has go to wait!")
+elif light == "red":
+    print("Car has to stop!")
+else:
+    print("unrecgonized signal!")
